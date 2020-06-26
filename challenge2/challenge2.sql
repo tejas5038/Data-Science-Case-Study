89c6eae59830d6df88bcff647f7c5663a24869e2
tejas <tejas5038@gmail.com>
2020-06-25 20:14:22 -0500

    Modified query

--- challenge2/challenge2.sql
+++ challenge2/challenge2.sql
@@ -1,7 +1,7 @@
 SELECT manager.first_name , manager.last_name FROM table1
     JOIN
-    manager ON table1.id_manager= manager.id_manager WHERE table1.dt_work_from='20200101 00:00:00' and table1.dt_work_to='20200131'
+    manager ON table1.id_manager= manager.id_manager WHERE table1.dt_work_from='20200101 00:00:00' and table1.dt_work_to='20200131 00:00:00'
 UNION ALL
 SELECT employee.first_name , employee.last_name FROM table1 
     JOIN
-    employee ON table1.id_employee= employee.id_employee WHERE table1.dt_work_from='20200101 00:00:00' and table1.dt_work_to='20200131'
+    employee ON table1.id_employee= employee.id_employee WHERE table1.dt_work_from='20200101 00:00:00' and table1.dt_work_to='20200131 00:00:00'
